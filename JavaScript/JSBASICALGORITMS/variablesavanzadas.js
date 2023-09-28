754b5df9d6ce827eb6f069a54725559a45bf8de1
Elena <elenagocu@gmail.com>
2023-09-28 21:05:36 +0200

    sin terminar

---
+++ JavaScript/JSBASICALGORITMS/variablesavanzadas.js
@@ -0,0 +1,9 @@
+const character = {name: 'Jack Sparrow', age: 10};
+let firstName = "Jon";
+let lastName = "Snow";
+let age = "24";
+console.log (`Soy ${firstName}, tengo ${age} años y me gustan los lobos.`)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log ()
