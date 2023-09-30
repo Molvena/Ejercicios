9a0f28490c87d4e8e06c8c01d6513b3257c62836
Elena <elenagocu@gmail.com>
2023-09-30 18:34:37 +0200

    Hasta Arrays

--- JavaScript/JSBASICALGORITMS/variables.js
+++ JavaScript/JSBASICALGORITMS/variables.js
@@ -4,3 +4,4 @@ let h =5
 let y = 10
 let z = h + y
 console.log (z)
+
--- JavaScript/JSBASICALGORITMS/variablesavanzadas.js
+++ JavaScript/JSBASICALGORITMS/variablesavanzadas.js
@@ -1,4 +1,8 @@
 const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+console.log (character);
+
+
 let firstName = "Jon";
 let lastName = "Snow";
 let age = "24";
@@ -6,4 +10,35 @@ console.log (`Soy ${firstName}, tengo ${age} años y me gustan los lobos.`)
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
-console.log ()
+console.log (toy1.price + toy2.price);
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+car1.finalPrice = (car1.basePrice + globalBasePrice);
+console.log (car1.finalPrice);
+
+
+let x = 10;
+let y = 5;
+console.log (x*y);
+
+let a = 2;
+console.log (x/a);
+
+let b = 15;
+let d = 9;
+console.log (b%d);
+
+let p = 10;
+let j = 5;
+const o = (p+j);
+console.log (o);
+
+let c = 10;
+let m = 5;
+const i = (c*m);
+console.log (i);
+
+
